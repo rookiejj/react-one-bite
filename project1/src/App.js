5df143a65419f3a5b1f1a634f5003c3f8d5dfd44
sean park <rookiejj@gmail.com>
2024-04-24 01:37:30 +0900

    project completed

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { useState } from "react";
+import Controller from "./component/Controller";
+import Viewer from "./component/Viewer";
 
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller handleSetCount={handleSetCount} />
+      </section>
+    </div>
+  );
 }
 
 export default App;
