7139ec6f8fcd0bbcefdabe94bb6ba492ee38b2c4
sean park <rookiejj@gmail.com>
2024-04-24 16:26:03 +0900

    project2 initial

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Controller from "./component/Controller";
 import Viewer from "./component/Viewer";
+import Even from "./component/Even";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,11 +10,39 @@ function App() {
     setCount(count + value);
   };
 
+  const didMountRef = useRef(false);
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+      return;
+    } else {
+      console.log("컴포넌트 업데이트");
+    }
+  });
+
+  useEffect(() => {
+    console.log("컴포넌트 마운트");
+  }, []);
+
+  useEffect(() => {
+    console.log("intervalID 컴포넌트 업데이트");
+    const intervalID = setInterval(() => {
+      console.log("깜빡", intervalID);
+    }, 1000);
+
+    return () => {
+      console.log("클린업", intervalID);
+      clearInterval(intervalID);
+    };
+  });
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <div>짝수 아님</div> : <Even />}
       </section>
       <section>
         <Controller handleSetCount={handleSetCount} />
