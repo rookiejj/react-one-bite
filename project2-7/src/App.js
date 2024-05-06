66e17697f9699cd82b5e330d9ed27f9ff845789f
sean park <rookiejj@gmail.com>
2024-05-07 01:45:48 +0900

    7장 [할 일 관리] 앱 업그레이드 추가

--- project2-7/src/App.js
+++ project2-7/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
 import TodoList from "./component/TodoList";
-import TestComp from "./component/TestComp";
 
 const mockTodo = [
   {
@@ -53,7 +52,6 @@ function App() {
 
   return (
     <div className="App">
-      <TestComp />
       <Header />
       <TodoEditor onCreate={onCreate} />
       <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
---
+++ project2-7/src/component/TodoList.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+import "./TodoList.css";
+
+const TodoList = ({ todo, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const getSearchResult = () => {
+    return search === ""
+      ? todo
+      : todo.filter((it) =>
+          it.content.toLowerCase().includes(search.toLowerCase())
+        );
+  };
+  return (
+    <div className="TodoList">
+      <h4>Todo List 🌱</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        className="searchbar"
+        placeholder="검색어를 입력하세요"
+      ></input>
+      <div className="list_wrapper">
+        {getSearchResult().map(function it(it) {
+          return (
+            <TodoItem
+              key={it.id}
+              {...it}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+        {/* {getSearchResult().map((it) => (
+          <TodoItem key={it.id} {...it} />
+        ))} */}
+      </div>
+    </div>
+  );
+};
+export default TodoList;
