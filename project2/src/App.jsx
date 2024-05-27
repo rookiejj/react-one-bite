67aa7824bfb6e94ecae0b46441d90d8b17d49a68
sean park <rookiejj@gmail.com>
2024-05-27 20:30:50 +0900

    todo 추가하기

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
@@ -27,13 +27,26 @@ const mockDate = [
 
 function App() {
   const [todos, setTodos] = useState(mockDate);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
   return (
     <div className="App">
       <section>
         <Header />
       </section>
       <section>
-        <Editor />
+        <Editor onCreate={onCreate} />
       </section>
       <section>
         <List />
