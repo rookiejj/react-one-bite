60b9c3dbbdca5f4a9fccef27c34c25fac83db880
sean park <rookiejj@gmail.com>
2024-06-01 23:50:56 +0900

    modify reducer function

--- reducer/src/App.jsx
+++ reducer/src/App.jsx
@@ -3,7 +3,6 @@ import { useState, useRef } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
-import Exam from "./components/Exam";
 
 const mockDate = [
   {
