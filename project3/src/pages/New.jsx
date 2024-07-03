2d435f301846ad25bea04629881b967891e93e54
sean park <rookiejj@gmail.com>
2024-07-03 17:44:02 +0900

    New 페이지 UI 구현

--- project3/src/pages/New.jsx
+++ project3/src/pages/New.jsx
@@ -1,9 +1,20 @@
 import usePageTitle from "../hooks/usePageTitle";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
 
 const New = () => {
   usePageTitle("새 일기 쓰기");
 
-  return <div>New</div>;
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로 가기"} />}
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default New;
