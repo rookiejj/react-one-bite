4f01774ae575de011fcb75b38e6b1e2578392b0b
sean park <rookiejj@gmail.com>
2024-07-03 23:15:04 +0900

    New 페이지 기능 구현

--- project3/src/pages/New.jsx
+++ project3/src/pages/New.jsx
@@ -2,17 +2,28 @@ import usePageTitle from "../hooks/usePageTitle";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const New = () => {
   usePageTitle("새 일기 쓰기");
+
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+  };
 
   return (
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={<Button text={"< 뒤로 가기"} />}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
